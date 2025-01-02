ee6964cf87b0f189328acb290c9b1c4b26c2e65e
LORD111051 <nurboyevamunira44@gmail.com>
2025-01-02 17:36:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let fruits = ["Яблоко", "банан", "Апелсин"];
+
+for (let element of fruits) {
+    console.log(element)
+}
+
+fruits.sort();
+console.log(fruits);
+
+let numebr = [1,2,3,4,5]
+numebr.reverse();
+console.log(numebr);
